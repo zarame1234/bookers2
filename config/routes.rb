21a3924a66c4fb70b35daf9a78626353436b9687
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-06 10:37:05 +0000

    [Add]コントローラー、モデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+  root to: 'homes#top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
