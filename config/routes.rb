127caef02500686fd5fe09ee05d50719734a51b3
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-08 23:34:19 +0000

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  devise_for :users
+  get 'home/about' =>'homes#about'
   get 'users/edit'
   get 'books/new'
   get 'books/index'
   get 'books/show'
   get 'books/edit'
-  root to: 'homes#top'
-  devise_for :users
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
