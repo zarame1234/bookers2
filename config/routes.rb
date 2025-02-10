bbc66c8b9a0b7a53ce850d8887ee3eb2cde97bbf
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-10 03:22:21 +0000

    [Add]userモデルの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' =>'homes#about'
   resources :books
+  resources :users, only: [:show, :edit]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
