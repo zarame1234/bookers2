85a9c526d5cd536f2a6bf20d4549a76bc2d608a3
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-10 05:56:07 +0000

    [Add]Userの編集更新機能作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,24 @@
 class UsersController < ApplicationController
-  def edit
-  end
+ 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
+
+  def edit
+    @user= User.find(params[:id])
+  end
+
+  def Update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:name, :profile_image, :introduction)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' =>'homes#about'
   resources :books
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
