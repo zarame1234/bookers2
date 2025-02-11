9fe9c57ca9e0f6be2fc208cd725b351880b2d040
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-11 13:21:16 +0000

    [update}

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
- # def new
-    #@book = Book.new
-  #end
 
   def create
     @book = Book.new(book_params)
@@ -16,13 +13,22 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
+  end
+
+
   def destroy
-    book = Book.fibd(params[:id])
+    book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   get 'home/about' =>'homes#about'
-  resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
+  resources :books
   resources :users, only: [:index, :show, :edit, :update]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
