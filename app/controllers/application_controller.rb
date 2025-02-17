53ba25a1aab679a563da820c148a7a2e86d61b45
zarame1234 <193503385+zarame1234@users.noreply.github.com>
2025-02-17 07:37:05 +0000

    [Update}課題提出後の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 end
